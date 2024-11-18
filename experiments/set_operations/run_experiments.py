4ee7ddc8fb3829dfa1c4b78702cbb1f3bcc6ac74
Pavel Vesely <vesely@iuuk.mff.cuni.cz>
2024-11-18 11:37:35 +0100

    little cleanup

--- experiments/set_operations/run_experiments.py
+++ experiments/set_operations/run_experiments.py
@@ -1,16 +1,13 @@
 import subprocess
 import os
 
-msi_path = "../../FMSIv0.2/fmsi"
+msi_path = "../../fmsi/fmsi"
 camel_path = "../../kmercamel/kmercamel"
-#cbl_path = "../../CBL/target/release/examples/cbl"
 cbl_path_pref = "../../CBL/target.k_"
 cbl_path_suff = "/release/examples/cbl"
 cbl_directory = "../../CBL"
-#file1 = "SARS-CoV-2-human-USA-NJ-CDC-LC1086226-2024.fa"
-#file2 = "SARS-CoV-2-human-USA-WA-CDC-UW24011298084-2024.fa"
-file1="C.briggsae.fna" #"Spneumoniae.fa"
-file2="C.elegans.fna" #"Spyogenes.fa"
+file1="C.briggsae.fna"
+file2="C.elegans.fna"
 
 def run_subprocess(args, offset=0):
     print(f"running {args}")
@@ -24,6 +21,7 @@ def run_subprocess(args, offset=0):
 
 def jellycount(file1, k):
     args = ["jellyfish", "count", "-m", str(k), "-s", "100M",  "-o", f"{file1}.jf" ,"-C", file1]
+    print(f"running {args}")
     subprocess.run(args)
     with open(f"{file1}_stats.txt", "w") as f:
         subprocess.run(["jellyfish", "stats", f"{file1}.jf"], stdout=f)
@@ -31,6 +29,7 @@ def jellycount(file1, k):
         for _ in range(4):
             key, value = f.readline().split()
             if key == "Distinct:":
+                print(f" ... found {value} distinct k-mers")
                 return int(value)
 
 
@@ -97,18 +96,18 @@ def measure(file1_orig: str, file2_orig: str, k: int, function: str):
     len2, runs2, ones2 = superstring_params(file2)
 
     time_indexing1, _ = run_subprocess([msi_path, "index", "-p", file1, "-k", str(k)], 5)
-    _, memory_1_query = run_subprocess([msi_path, "query", "-p", file1, "-k", str(k), "-q", query_file], 0)
+    _, memory_1_query = run_subprocess([msi_path, "query", "-p", file1, "-k", str(k), "-q", query_file, "-s"], 0)
     print("constructed index on file1")
     time_indexing2, _ = run_subprocess([msi_path, "index", "-p", file2, "-k", str(k)], 5)
-    _, memory_2_query = run_subprocess([msi_path, "query", "-p", file2, "-k", str(k), "-q", query_file], 0)
+    _, memory_2_query = run_subprocess([msi_path, "query", "-p", file2, "-k", str(k), "-q", query_file, "-s"], 0)
     print("constructed index on file2")
 
     time_merge, _ = run_subprocess([msi_path, "merge", "-p", file1, "-p", file2,  "-r", result], 3)
-    _, memory_merge_query = run_subprocess([msi_path, "query", "-p", result, "-k", str(k), "-q", query_file], 0)
+    _, memory_merge_query = run_subprocess([msi_path, "query", "-p", result, "-k", str(k), "-q", query_file, "-f", function, "-s"], 0)
     print("ran merge")
 
     time_normalize, _ = run_subprocess([msi_path, "normalize", "-p", result, "-k", str(k), "-f", function], 4)
-    _, memory_normalize_query = run_subprocess([msi_path, "query", "-p", result, "-k", str(k), "-q", query_file], 0)
+    _, memory_normalize_query = run_subprocess([msi_path, "query", "-p", result, "-k", str(k), "-q", query_file, "-s"], 0)
     print("ran normalize")
 
     subprocess.run([msi_path, "export", "-p", result], stdout=open(result, "w"))
@@ -116,12 +115,13 @@ def measure(file1_orig: str, file2_orig: str, k: int, function: str):
 
     return time_indexing1, time_indexing2, time_merge, time_normalize, memory_1_query, memory_2_query, memory_merge_query, memory_normalize_query, len1, len2, len_res, runs1, runs2, runs_res, ones1, ones2, ones_res
 
-
+# with open("results-test.csv", "w") as output:
 with open("results-roundworms.csv", "w") as output:
     output.write("dataset1,dataset2,k,operation,indexing time 1(s),indexing time 2(s),merge time(s),normalization time(s),set 1 query memory(kB),set 2 query memory(kB),merge query memory(kB),normalization query memory(kB),ms 1 length,ms 2 length,result ms length,ms 1 runs,ms 2 runs,result ms runs,ms 1 ones,ms 2 ones,result ms ones,kmers1,kmers2,kmersRes\n")
 
     K_VALS = list(range(17, 23, 2))
     K_VALS.append(31)
+    K_VALS = [15]
     for k in K_VALS:
         print(f"Running k={k}")
         kmers = count_kmers(file1, file2, k)
